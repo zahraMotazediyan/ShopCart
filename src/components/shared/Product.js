cb66acbf3d5579b61eab27ca7bf91fb8a355bd2e
Zahra Motazediyan <zahra.motazediyan2019@gmail.com>
2023-07-16 23:20:17 +0330

    give props productData to file shared

--- src/components/shared/Product.js
+++ src/components/shared/Product.js
@@ -1,9 +1,17 @@
 import React from "react";
 
-const Product =()=>{
-    return(
-        <div></div>
-    )
+const Product = ({productData}) => {
+    return (<div>
+        <img src={productData.image} alt="product" style={{width:"200px"}}/>
+        <h3>{productData.title}</h3>
+        <p>{productData.price}</p>
+        <div>
+            <a>Details</a>
+            <div>
+                <button>Add to Cart</button>
+            </div>
+        </div>
+    </div>)
 }
 
 export default Product;
